8fd62e37ee319702476f860b79bfe988bd2751f3
johnnyauto <jonney211@gmail.com>
2024-02-21 19:59:35 +0800

    2024.02.21_Update 2
    1. update for process the contents of ValTable
    2. update for EMMC_BYTE_# signals
    3. update for some special case in signal name

--- exlDBC.py
+++ exlDBC.py
@@ -2,13 +2,33 @@ import pandas as pd
 import openpyxl
 #import re
 
+##### fun(): check for special cases in signal names #####
+def chk_signalname(sigName):
+    match sigName:
+        case 'AutoHiLowBeamSwtSts_HCMAutoHiLowBeamSwtSts':
+            sigName = 'AutoHiLowBeamSwtSts'
+        case 'HUT_TurnLeftLightReqTurnLeftLightReq_HUT':
+            sigName = 'TurnLeftLightReq_HUT'
+        case 'HUT_TurnRightLightReqTurnRightLightReq_HUT':
+            sigName = 'TurnRightLightReq_HUT'
+        case 'DomeLampDelayTimeSetDomelmpDlyTimSet_HUT':
+            sigName = 'DomelmpDlyTimSet_HUT'
+        case 'CEM_FrntWinHeatEnaStsFrntWinHeatEnaSts':
+            sigName = 'FrntWinHeatEnaSts'
+        case _:
+            pass
+    return sigName
+
 ##### fun(): process val for SG_ #####
 def sg_value(group_data, colName, dataIndex):
     val = group_data[colName].iloc[dataIndex]
     val = str(val).replace("\n(0xFF)","")   # remove "\n(0xFF)"
     # convert empty data to 0
     if pd.isna(val) or val == 'nan' or val == 'None':
-        val = 0
+        if colName == 'Factor':
+            val = 1
+        else:
+            val = 0
     else:
         pass
     return val
@@ -19,6 +39,7 @@ def gen_dbc(sheet_name, bus_type, il_support):
 
     # Process Data (remove Empty and Strikethrough format data) 
     pData = []      # processed Data
+    Sig_index = 6   # column index of 'Signal Name'
     Msg_index = 10  # column index of 'Message Name'
     Mab_index = 20  # column index of 'Mab'
 
@@ -27,8 +48,9 @@ def gen_dbc(sheet_name, bus_type, il_support):
         Msg_value = worksheet.cell(row=row_index, column=Msg_index).value
         Mab_value = worksheet.cell(row=row_index, column=Mab_index).value
         Msg_strike = worksheet.cell(row=row_index, column=Msg_index).font.strike
+        Sig_strike = worksheet.cell(row=row_index, column=Sig_index).font.strike
 
-        if Msg_value and Mab_value and not Msg_strike:
+        if Msg_value and Mab_value and not Msg_strike and not Sig_strike:
             # generate a processed Data
             pData.append([worksheet.cell(row=row_index, column=col).value for col in range(1, worksheet.max_column + 1)])
 
@@ -151,7 +173,18 @@ def gen_dbc(sheet_name, bus_type, il_support):
 
             # SG_ {signal_name} {multiplexer_indicator} : {start_bit}|{signal_size}@{byte_order}{value_type} ({factor},{offset}) [{minimum}|{maximum}] "{unit}" {receiver}
             if not 'EMMC_BYTE_' in signal_name:
+                signal_name = chk_signalname(signal_name)
                 output_seg03 += f' SG_ {signal_name} {multiplexer_indicator}: {start_bit}|{signal_size}@{byte_order}{value_type} ({factor},{offset}) [{minimum}|{maximum}] "{unit}" {receiver}\n'
+            # only for EMMC_BYTE_# signals
+            else:
+                signal_name = signal_name.replace('EMMC_BYTE_','')
+                signal_name_split = signal_name.split("~")
+                start_bit = 7
+                for number in range(int(signal_name_split[0]), int(signal_name_split[1])+1):
+                    signal_name = f'EMMC_BYTE_{number}'
+                    signal_size = 8
+                    output_seg03 += f' SG_ {signal_name} {multiplexer_indicator}: {start_bit}|{signal_size}@{byte_order}{value_type} ({factor},{offset}) [{minimum}|{maximum}] "{unit}" {receiver}\n'
+                    start_bit = start_bit+8
 
             if dataIndex == len(group_data)-1:
                 output_seg03 += '\n'
@@ -233,10 +266,9 @@ def gen_dbc(sheet_name, bus_type, il_support):
             # A flag used to determine whether to generate a ValTable
             gen_ValTable = True
 
-            # Create a value table for signals whose Unit is empty
-            if pd.isna(group_data['Unit'].iloc[dataIndex]):
-                ValTable = group_data['Coding'].iloc[dataIndex]
-
+            ValTable = group_data['Coding'].iloc[dataIndex]
+            # if ValTable is not empty, create a value table for the signal
+            if not pd.isna(ValTable):
                 # process the contents of ValTable
                 ValTable = ValTable.replace(' : ',':')
                 ValTable = ValTable.replace('：',':')
@@ -249,59 +281,58 @@ def gen_dbc(sheet_name, bus_type, il_support):
                 ValTable = ValTable.replace('"','')
                 ValTable = ValTable.replace('0x16-1F','0x16~1F')
                 ValTable = ValTable.replace(' 0x','\n0x')   # SAS_Sts (CAN01_Matrix\INT3)
-            
-                # if ValTable is not empty
-                if not pd.isna(ValTable):
-                    signal_name = group_data['Signal Name'].iloc[dataIndex]
-                    signal_name = signal_name.replace(' ','')
-                    signal_name = signal_name.replace('\n','')
-                    signal_name = signal_name.replace('(PS:自定义)','')
-                    
-                    
-                    # split the ValTable by '\n' and save the results to a list
-                    ValTable_split = ValTable.split("\n")
 
-                    Value_Description = ''
-                    for data in ValTable_split:
-                        r"""
-                        if '~' in data:     
-                            # use regular expression to separate data (0xAA~0xBB:CCCC) into three parts:'0xAA', '0xBB' and 'CCCC'
-                            pattern = re.compile(r'(\w+x\w+)~(\w+x\w+):(\w+)')
-                            regular_data = pattern.match(data)
-                            start, end, Description = regular_data.groups()
-                            for Value in range(int(start, 16), int(end, 16)+1):
-                                Value_Description = f'{Value} "{Description}" '
-                                output_seg06 += f'VAL_ {message_id} {signal_name} {Value_Description};\n'
-                            
-                            # do not porcess data containing '~', because the range of some value is too large
-                            gen_ValTable = False
-                            continue
-                        elif 'EMMC_BYTE_' in signal_name:
-                        """
+                # process the signal_name
+                signal_name = group_data['Signal Name'].iloc[dataIndex]
+                signal_name = signal_name.replace(' ','')
+                signal_name = signal_name.replace('\n','')
+                signal_name = signal_name.replace('(PS:自定义)','')
+                signal_name = chk_signalname(signal_name)
+                
+                # split the ValTable by '\n' and save the results to a list
+                ValTable_split = ValTable.split("\n")
 
-                        if 'EMMC_BYTE_' in signal_name:
-                            # ValTable is not generated when the signal name contains "EMMC_BYTE_"
-                            gen_ValTable = False
+                Value_Description = ''
+                for data in ValTable_split:
+                    r"""
+                    if '~' in data:     
+                        # use regular expression to separate data (0xAA~0xBB:CCCC) into three parts:'0xAA', '0xBB' and 'CCCC'
+                        pattern = re.compile(r'(\w+x\w+)~(\w+x\w+):(\w+)')
+                        regular_data = pattern.match(data)
+                        start, end, Description = regular_data.groups()
+                        for Value in range(int(start, 16), int(end, 16)+1):
+                            Value_Description = f'{Value} "{Description}" '
+                            output_seg06 += f'VAL_ {message_id} {signal_name} {Value_Description};\n'
+                        
+                        # do not porcess data containing '~', because the range of some value is too large
+                        gen_ValTable = False
+                        continue
+                    elif 'EMMC_BYTE_' in signal_name:
+                    """
+
+                    if 'EMMC_BYTE_' in signal_name:
+                        # ValTable is not generated when the signal name contains "EMMC_BYTE_"
+                        gen_ValTable = False
+                    else:
+                        # split data to two elements (Value & Description) by ':'
+                        data_split = data.split(":", 1)
+
+                        # due to some data lacks ':', use " " to split instead
+                        if len(data_split) == 1:
+                            data_split = data.split(" ", 1)
+                        
+                        # do not porcess data containing '~'
+                        if '~' in data_split[0]:
+                            Value =''
+                            Description =''
                         else:
-                            # split data to two elements (Value & Description) by ':'
-                            data_split = data.split(":", 1)
-
-                            # due to some data lacks ':', use " " to split instead
-                            if len(data_split) == 1:
-                                data_split = data.split(" ", 1)
-                            
-                            # do not porcess data containing '~'
-                            if '~' in data_split[0]:
-                                Value =''
-                                Description =''
-                            else:
-                                Value = int(data_split[0], 16)  # convert Hex to Dec
-                                Description = f'"{data_split[1]}" '
-                            Value_Description += f'{Value} {Description}'
-                    
-                    # if gen_ValTable is True, generates ValTable
-                    if gen_ValTable:
-                        output_seg06 += f'VAL_ {message_id} {signal_name} {Value_Description};\n'
+                            Value = int(data_split[0], 16)  # convert Hex to Dec
+                            Description = f'"{data_split[1]}" '
+                        Value_Description += f'{Value} {Description}'
+                
+                # if gen_ValTable is True, generates ValTable
+                if gen_ValTable:
+                    output_seg06 += f'VAL_ {message_id} {signal_name} {Value_Description};\n'
 
 
     # create a DBC file
@@ -310,7 +341,7 @@ def gen_dbc(sheet_name, bus_type, il_support):
     with open(DBC_name, 'w', encoding='utf-8') as f:
         f.write(output_text)
     print(f'\n{DBC_name} is generated!!\n')
-    input('Press [Enter] to continue.\n')
+    input('Press [Enter] to continue.')
     
 
 ##### Main #####
@@ -328,20 +359,18 @@ while True:
         input('Press [Enter] to continue.\n')
 
 while True:
-    # get sheet name for workbook and ptint it
+    # get sheetName and sheet_index from workbook
     sheet_name_list = workbook.sheetnames
-    print('\nSheet list:')
+    print('\n\n[Sheet list]')
     for index, sheetName in enumerate(sheet_name_list):
         print(f'{index}: {sheetName}')
-    
     sheet_index = input("選擇一個sheet(輸入數字)生成DBC, 或輸入'q'結束程式: ")
-    
     if sheet_index.lower() == 'q':
         break
 
     # determine the BusType attribute
     else:
-        print('\nCAN bus types:\n1. CAN (default)\n2. CAN FD')
+        print('\n\n[CAN bus types]\n1. CAN (default)\n2. CAN FD')
         busType = input("選擇CAN bus類別(輸入數字), 或輸入'q'結束程式: ")
         if busType == '2':
             busType = 'CAN FD'
@@ -351,7 +380,7 @@ while True:
             busType = 'CAN'
 
         # Determine the GenMsgILSupport attribute
-        print('\nGenMsgILSupport:\n1. No (default)\n2. Yes')
+        print('\n\n[GenMsgILSupport]\n1. No (default)\n2. Yes')
         print('(若啟用GenMsgILSupport, CANoe會根據Attribute定義自動發送CAN message.)')
         ILSupport = input("選擇是否啟用GenMsgILSupport(輸入數字), 或輸入'q'結束程式:")
         if ILSupport == '2':
@@ -368,3 +397,5 @@ while True:
         except:
             print('\nError! 請確認所選擇的sheet內容是否正確')
             input('Press [Enter] to continue.\n')
+
+
